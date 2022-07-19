69834666d7a0a07b328fcba53e5ab996a5352e04
phungson185 <phungson185@gmail.com>
2022-07-19 22:41:11 +0700

    fix: message add and edit

--- src/pages/CreateCourse.jsx
+++ src/pages/CreateCourse.jsx
@@ -462,7 +462,7 @@ const Content = (props) => {
         <Grid item xs={8} />
         <Grid item xs={4}>
           <Button size="large" variant="contained" onClick={submit}>
-            提出
+            追加
           </Button>
         </Grid>
       </Grid>
